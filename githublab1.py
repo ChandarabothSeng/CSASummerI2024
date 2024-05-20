32de3ab0e5dec64bb61327c51d424ad06bd44261
ChandarabothSeng <144997059+ChandarabothSeng@users.noreply.github.com>
2024-05-20 14:38:25 +0700

    commit

--- githublab1.py
+++ githublab1.py
@@ -2,3 +2,13 @@
 # Sample run:
 # Enter the number of minutes:  1000000000
 # 1000000000 minutes is approximately 1902 years and 214 days
+
+user_input = int(input("Enter minutes: "))
+
+total_days = user_input//(60*24)
+
+years = total_days//365
+
+remaining_days = total_days%365
+
+print(f"{user_input} minutes is approximately {years} years {remaining_days} days.")
